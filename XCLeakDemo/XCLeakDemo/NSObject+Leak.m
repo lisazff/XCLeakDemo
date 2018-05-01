1ff1ff1a1ce28b249c4d55a5dff2fa79958dff39
Cai Tengyuan <caitengyuan@baidu.com>
2018-05-01 16:38:56 +0800

    添加注释

--- XCLeakDemo/XCLeakDemo/NSObject+Leak.m
+++ XCLeakDemo/XCLeakDemo/NSObject+Leak.m
@@ -24,6 +24,7 @@
 }
 
 - (BOOL)willDealloc{
+    // 这里注意, 使用weakSelf 防止引起循环引用
     __weak typeof(self) weakSelf =  self;
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         __strong typeof(weakSelf) strongSelf = weakSelf;
